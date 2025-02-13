1661dc5fb09fa97f2323f9fd3e769f365468223a
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2025-02-13 10:18:22 +0100

    day2 exercise

---
+++ day2/exercise.py
@@ -0,0 +1,15 @@
+import sys
+# import platform
+"""
+Write a Python program to find out what version of Python you are using.
+
+solution
+import sys module to get version the version of a  python on any os
+you can also use platform module to get the version of python on any os
+"""
+
+
+
+print(f'The python version running on this OS is: ', sys.version)
+
+# print(platform.python_version())
