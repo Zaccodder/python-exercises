7ce112812cf0b7707a45703b7a435eb39b4ae9ca
Namujibril <Namujibril@gmail.com>
2025-02-13 12:28:29 +0000

    checking the version of python

---
+++ Day2/Exercise2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 2. Python Version Checker
+# Write a Python program to find out what version of Python you are using.
+
+# In[ ]:
+
+
+import sys
+
+print(f'The current version is:', sys.version)
+
+
+# ### Explanations
+# The code imports the "sys" module and uses it to print the version of Python currently being used.
